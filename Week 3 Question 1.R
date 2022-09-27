f2f2becbe9ebecc5a24b10bc3598dd94a177f33a
jtbriggs12 <jbriggs5388@gmail.com>
2022-09-27 13:43:58 -0500

    Share with Amanda

---
+++ Week 3 Question 1.R
@@ -0,0 +1,9 @@
+#Title: Stat 955 Homework 3
+#Authors: Jess Briggs & Amanda Kerkhove
+#Date Created:2022-09-27
+
+#Background
+#Black sea bass are a protogynous hermaphrodite marine fish species – they change sex from female to male, although there are also primary males which first mature as males.  They do not change from male to female and they change sex primarily after the end of the spawning season (May to July).  The file BSB_tagging_data.xlsx contains the results of a tagging study conducted on this species.  Individual black sea bass (BSB) were captured, tagged with a unique identifier, and released on the “Date_at_capture.”  Some of these individuals were recaptured on the “Date_at_recapture.”  Sex and length were recorded at both time points (though this file only includes the “Length_at_capture.”  Sex change isn’t instantaneous, so some individuals recaptured while undergoing sex change are recorded as intersex.  These individuals can also be thought of as having changed sex.
+
+
+#Question 1.1: Plot a probability density function (probability density vs. proportions from 0 to 1) for the proportion of female black sea bass that changed sex out of all those which were recaptured after the end of the spawning season (i.e., after July). 
