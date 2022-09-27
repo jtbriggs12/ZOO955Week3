bf91b06646e955369ca5daf80a963c3f9e7d6ea8
Amanda Kerkhove <kerkhoveamanda@gmail.com>
2022-09-27 14:37:28 -0500

    Amanda

--- Week 3 Question 1.R
+++ Week 3 Question 1.R
@@ -7,3 +7,29 @@
 
 
 #Question 1.1: Plot a probability density function (probability density vs. proportions from 0 to 1) for the proportion of female black sea bass that changed sex out of all those which were recaptured after the end of the spawning season (i.e., after July). 
+library(lubridate)
+library(dplyr)
+data<-read.csv("BSB_tagging_data.csv")%>%
+  mutate(Date_at_recapture = mdy(Date_at_recapture))%>%
+  filter(month(Date_at_recapture) != 5)%>%
+  filter(month(Date_at_recapture) !=6)%>%
+  filter(month(Date_at_recapture) != 4)%>%
+  mutate(Sex_at_recapture = as.factor(Sex_at_recapture))
+
+recapFemale<-data%>%
+  group_by(Sex_at_recapture)%>%
+  count()
+density<-dbeta(data, shape1 = 42, shape2 = 68)
+
+
+bData<-data%>%
+  select(Sex_at_recapture)
+bData<-ifelse(bData$Sex_at_recapture == "F", 1, 0)
+
+x_beta <- seq(0, 1, by = 0.02)    
+dbeta<-dbeta(x_beta, 42, 68)
+plot(x_beta, dbeta)
+
+
+
+
